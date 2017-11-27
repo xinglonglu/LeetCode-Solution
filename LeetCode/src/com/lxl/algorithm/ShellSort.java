dc1f813d5cfca0783a0b5803f7514cebcabb8683
lxlWORK <460668853@qq.com>
2017-11-27 10:57:38 +0800

    希尔排序

--- LeetCode/src/com/lxl/algorithm/ShellSort.java
+++ LeetCode/src/com/lxl/algorithm/ShellSort.java
@@ -6,15 +6,12 @@ public void sort(int a[] , int n){
 		for ( gap = n/2; gap > 0; gap /= 2) {
 			for ( i = 0 ;i<gap; i++) {
 				 for(j= i+gap;j<n;j+=gap){
-					 if(a[j]<a[j-gap]){
-						 int temp = a[j];
-						 int k = j-gap;
-						 while(k>=0&&a[k]>temp){
-							 a[k+gap]=a[k];
-							 k-=gap;
-						 }
-						 a[k+gap]=temp;
-					 }
+					 int k = j-gap;
+					 int temp=a[j];
+					 for (; k>=0&&a[k]>temp;k-=gap) {
+						a[k+gap]=a[k];
+					}
+					 a[k+gap]=temp;
 				 }
 			}
 		}

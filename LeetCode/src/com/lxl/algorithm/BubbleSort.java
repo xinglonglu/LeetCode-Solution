b7add79bafaeef26423ed0a9f4bc39ce97e02999
lxlWORK <460668853@qq.com>
2017-11-27 10:50:45 +0800

    冒泡

--- LeetCode/src/com/lxl/algorithm/BubbleSort.java
+++ LeetCode/src/com/lxl/algorithm/BubbleSort.java
@@ -4,9 +4,9 @@ public class BubbleSort {
 public void sort (int[] numbers){
 	int n =  numbers.length-1 ;
 	int t;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n-i; j++) {
-			if(numbers[j]<numbers[j+1]){
+	for (int i = n; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			if(numbers[j]>numbers[j+1]){
 				t=numbers[j];
 				numbers[j]=numbers[j+1];
 				numbers[j+1]=t;

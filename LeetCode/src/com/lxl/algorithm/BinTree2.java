10124924954dbb4f7740cb4ff1b0ab37af6722b9
lxlWORK <460668853@qq.com>
2017-11-30 11:05:57 +0800

    非递归

--- LeetCode/src/com/lxl/algorithm/BinTree2.java
+++ LeetCode/src/com/lxl/algorithm/BinTree2.java
@@ -2,6 +2,7 @@ package com.lxl.algorithm;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinTree2 {
       private int[] array = {1,2,3,4,5,6,7,8,9};
@@ -37,11 +38,63 @@ public class BinTree2 {
           System.out.print(node.date + " ");  
           preOrderTraverse(node.childleft);  
           preOrderTraverse(node.childright);  
-      }  
+      }
+      public static void preorder2(Node node){
+    	  Stack<Node> s = new Stack<Node>();
+    	  while(node !=null || !s.isEmpty()){
+    		  while(node!=null){
+    			  System.out.println(node.date);
+    			  s.push(node);
+    			  node=node.childleft;
+    		  }
+    		  if(!s.isEmpty()){
+    			  node=s.pop();
+    			  node=node.childright;
+    		  }
+    	  }
+      }
+      public static void inorder2(Node node){
+    	  Stack<Node> s = new Stack<Node>();
+    	  while(node !=null || !s.isEmpty()){
+    		  while(node!=null){
+    			  s.push(node);
+    			  node=node.childleft;
+    		  }
+    		  if(!s.isEmpty()){
+    			  node=s.pop();
+    			  System.out.println(node.date);
+    			  node=node.childright;
+    		  }
+    	  }
+      }
+      public static void PostOrder2(Node node) { 
+    	  Stack<Node> s = new Stack<Node>();
+    	  Stack<Integer> s2 = new Stack<Integer>();
+    	  Integer i = new Integer(1);
+    	  while(node !=null || !s.isEmpty()){
+    		  while(node!=null){
+    			  s.push(node);
+    			  s2.push(new Integer(0));
+    			  node=node.childleft;
+    		  }
+    		  while(!s.isEmpty()||s2.peek().equals(i)){
+    			  s2.pop();
+    			  System.out.println(s.pop().date);
+    		  }
+    		  if(!s.empty()){
+    			  s2.pop();
+    			  s2.push(new Integer(1));
+    			  node=s.peek();
+    			  node=node.childright;
+    		  }	  
+    	  }
+   	  
+      }
       public static void main(String[] args) {
     	  BinTree2 tree = new BinTree2();
     	  Node root = nodelist.get(0); 
     	  tree.createtree();
     	  tree.preOrderTraverse(root);
+    	  tree.preorder2(root);
 	}
 }
